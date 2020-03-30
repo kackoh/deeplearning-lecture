05c021da5c5f71fce3b5f7b02c421ab0b09cbab0
kackoh <kackoh40621@outlook.jp>
2020-03-30 16:09:33 +0900

    add api

---
+++ flask-api/apiserver.py
@@ -0,0 +1,13 @@
+import json
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__, static_folder='./build/static', template_folder='./build')
+
+@app.route('/api/cr', methods=['POST'])
+def test():
+    pic = request.get_json()['pic']
+    return jsonify(pic=pic)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000) #5000番に指定しておく・webpack.config.jsとpackage.jsonにproxy設定忘れない
