fb1ff22b14a0fdb6517e260e0510e66e832cd02a
kackoh <kackoh40621@outlook.jp>
2020-03-31 15:01:30 +0900

    add recognition

--- flask-api/apiserver.py
+++ flask-api/apiserver.py
@@ -1,4 +1,5 @@
 import json
+import recognize
 from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__, static_folder='./build/static', template_folder='./build')
@@ -6,7 +7,8 @@ app = Flask(__name__, static_folder='./build/static', template_folder='./build')
 @app.route('/api/cr', methods=['POST'])
 def test():
     pic = request.get_json()['pic']
-    return jsonify(pic=pic)
+    ans = recognize.get_predict(pic)
+    return jsonify(pic=pic, ans=ans[0])
 
 if __name__ == '__main__':
     app.debug = True
